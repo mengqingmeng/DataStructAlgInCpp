c34d8b29c0824afaa6d47d548656e7179505870b
mqm <762577099@qq.com>
2024-12-25 23:48:20 +0800

    cpp base

---
+++ CppBase/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template<class T>
+int count(T value,const int n) {
+	int count = 0;
+	n = n <= 1 ? 1 : n;
+	for (int i; i < n; i++) {
+
+	}
+}
+
+int main() {
+	std::cout << "cpp base main func" << std::endl;
+	return 1;
+}
